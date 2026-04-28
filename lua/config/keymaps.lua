99f3a1a41cff462f5dccb654fa094ffdfb6824a3
Carlavier <namto10042004@gmail.com>
2026-04-28 21:57:50 +0700

    [init] config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,16 @@
+local allModes = {'n', 'v', 's', 'x', 'o'}
+
+vim.keymap.set(allModes, 'j', 'h')
+vim.keymap.set(allModes, 'l', 'j')
+vim.keymap.set(allModes, ';', 'l')
+vim.keymap.set(allModes, 'h', ';')
+
+vim.keymap.set('n', '<C-z>', '<Nop>')
+
+vim.keymap.set('n', '<C-Right>', 'w', { noremap = true })
+vim.keymap.set('n', '<C-Left>', 'b', { noremap = true })
+
+vim.keymap.set('i', '<C-h>', '<C-w>')
+
+vim.keymap.set({"n", "v"}, "p", "gP")
+vim.keymap.set({"n", "v"}, "P", "p")
