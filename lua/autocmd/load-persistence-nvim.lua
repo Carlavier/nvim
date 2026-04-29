8efb2c9918cb9d451728cbdb2f6d3b168f38b1d3
Carlavier <namto10042004@gmail.com>
2026-04-29 20:12:50 +0700

    [sync] config

--- lua/autocmd/load-persistence-nvim.lua
+++ lua/autocmd/load-persistence-nvim.lua
@@ -1,6 +1,10 @@
 vim.api.nvim_create_autocmd('VimEnter', {
   group = vim.api.nvim_create_augroup('persistence', { clear = true }),
   callback = function()
-    if vim.fn.argc() == 0 then require('persistence').load() end
+    if vim.fn.argc() == 0 then
+      require('persistence').load()
+      -- Force filetype detection on all loaded buffers
+      vim.cmd('silent! bufdo filetype detect')
+    end
   end,
 })
