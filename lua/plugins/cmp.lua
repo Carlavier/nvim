4a7e37283aa4c1e90e535af6713657c5a26d2524
Carlavier <namto10042004@gmail.com>
2026-05-09 23:43:00 +0700

    [modify] wip lsp, workspace auto import not working

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -17,18 +17,6 @@ return {
           },
           documentation = { auto_show = true, auto_show_delay_ms = 200 },
       },
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = {
         preset = 'default',
         ['<Up>'] = { 'select_prev', 'fallback' },
@@ -57,13 +45,7 @@ return {
         },
     },
 
-    -- (Default) list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
-    sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
-
-    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-    -- You may use a lua implementation instead by using `implementation = "lua"`
-    -- See the fuzzy documentation for more information
+    sources = { default = { 'lsp', 'path', 'snippets' } },
     fuzzy = { implementation = "rust" }
   },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,16 +20,30 @@ return {
 
     {
         "mason-org/mason-lspconfig.nvim",
-        opts = {
-            ensure_installed = {
-                'ts_ls',
-                'html',
-                'cssls',
-                'tailwindcss',
-                'lua_ls',
-                'eslint',
-                'basedpyright',
-            }
-        }
+        dependencies = { "mason-org/mason.nvim", "neovim/nvim-lspconfig" },
+        config = function()
+            local lspconfig = require("lspconfig")
+
+            local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    'ts_ls',
+                    'html',
+                    'cssls',
+                    'tailwindcss',
+                    'lua_ls',
+                    'eslint',
+                    'basedpyright',
+                },
+                handlers = {
+                    function(server_name)
+                        lspconfig[server_name].setup({
+                            capabilities = capabilities,
+                        })
+                    end,
+                }
+            })
+        end
     }
 }
