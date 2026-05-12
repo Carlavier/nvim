1cd8e746ccc0d09b2df11f1fa24328caa4f06808
Carlavier <namto10042004@gmail.com>
2026-05-12 07:37:39 +0700

    [modify] conform auto format

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,53 +2,76 @@ local global_configs = {
   lua = {
     name = "stylua",
     filename = { "stylua.toml", ".stylua.toml" },
-    path = vim.fn.expand("~/.config/nvim/formatter/stylua.toml"),
-    config_flag = "--config-path",
+    found_args = { "--stdin-filepath", "$FILENAME", "-" },
+    fallback_args = {
+      "--config-path",
+      vim.fn.expand("~/.config/nvim/formatter/stylua.toml"),
+      "--stdin-filepath",
+      "$FILENAME",
+      "-",
+    },
   },
   cpp = {
     name = "clang-format",
     filename = { ".clang-format" },
-    path = vim.fn.expand("~/.config/nvim/formatter/.clang-format"),
-    config_flag = "--style=file:",
+    found_args = { "--style=file", "--assume-filename", "$FILENAME" },
+    fallback_args = {
+      "--style=file:" .. vim.fn.expand("~/.config/nvim/formatter/.clang-format"),
+      "--assume-filename",
+      "$FILENAME",
+    },
   },
   javascript = {
     name = "prettier",
     filename = { ".prettierrc", ".prettierrc.json", "prettier.config.js" },
-    path = vim.fn.expand("~/.config/nvim/formatter/.prettierrc"),
-    config_flag = "--config",
+    found_args = { "--stdin-filepath", "$FILENAME", "-" },
+    fallback_args = {
+      "--config",
+      vim.fn.expand("~/.config/nvim/formatter/.prettierrc"),
+      "--stdin-filepath",
+      "$FILENAME",
+      "-",
+    },
   },
   python = {
     name = "black",
     filename = { "pyproject.toml", "black" },
-    path = vim.fn.expand("~/.config/nvim/formatter/pyproject.toml"),
-    config_flag = "--config",
+    found_args = { "--stdin-filepath", "$FILENAME", "-" },
+    fallback_args = {
+      "--config",
+      vim.fn.expand("~/.config/nvim/formatter/pyproject.toml"),
+      "--stdin-filepath",
+      "$FILENAME",
+      "-",
+    },
+  },
+  sh = {
+    name = "shfmt",
+    filename = { ".editorconfig", ".shfmt" },
+    found_args = { "--stdin-filepath", "$FILENAME", "-" },
+    fallback_args = {
+      "--config",
+      vim.fn.expand("~/.config/nvim/formatter/.shfmt"),
+      "--stdin-filepath",
+      "$FILENAME",
+      "-",
+    },
   },
 }
 
+-- Map aliases
 global_configs.typescript = global_configs.javascript
 global_configs.typescriptreact = global_configs.javascript
 global_configs.javascriptreact = global_configs.javascript
+global_configs.css = global_configs.javascript
+global_configs.html = global_configs.javascript
 
 local function get_custom_args(config)
   return {
     command = config.name,
-    args = function(self, ctx)
-      local found = vim.fs.find(config.filename, {
-        path = ctx.dirname,
-        upward = true,
-      })[1]
-
-      if found then
-        if config.name == "clang-format" then
-          return { "--style=file", "--assume-filename", "$FILENAME" }
-        end
-        return { "--stdin-filepath", "$FILENAME", "-" }
-      else
-        if config.name == "clang-format" then
-          return { "--style=file:" .. config.path, "--assume-filename", "$FILENAME" }
-        end
-        return { config.config_flag, config.path, "--stdin-filepath", "$FILENAME", "-" }
-      end
+    args = function(_, ctx)
+      local found = vim.fs.find(config.filename, { path = ctx.dirname, upward = true })[1]
+      return found and config.found_args or config.fallback_args
     end,
   }
 end
@@ -59,21 +82,15 @@ return {
   cmd = { "ConformInfo" },
   opts = function()
     local custom_formatters = {}
-    for _, cfg in pairs(global_configs) do
+    local formatters_by_ft = {}
+
+    for ft, cfg in pairs(global_configs) do
       custom_formatters[cfg.name] = get_custom_args(cfg)
+      formatters_by_ft[ft] = { cfg.name }
     end
 
     return {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        cpp = { "clang-format" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        python = { "black" },
-        sh = { "shfmt" },
-      },
+      formatters_by_ft = formatters_by_ft,
       formatters = custom_formatters,
       format_on_save = {
         timeout_ms = 500,
